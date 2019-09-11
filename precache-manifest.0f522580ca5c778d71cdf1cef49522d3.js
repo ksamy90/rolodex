2eb408ecf903a9d6828ce4b5bbf4efe06c5fbab0
ksamy90 <ksamy90@aol.com>
2019-09-10 20:50:59 -0700

    Updates

---
+++ precache-manifest.0f522580ca5c778d71cdf1cef49522d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2bf9415ece7371eaf7c0d0fff3d128",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "b417bac673768f325480",
+    "url": "/rolodex/static/css/main.6b4398c9.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "b417bac673768f325480",
+    "url": "/rolodex/static/js/main.0a5ff13b.chunk.js"
+  },
+  {
+    "revision": "41f11689102a27bddf9e",
+    "url": "/rolodex/static/js/runtime~main.230a6f71.js"
+  }
+]);
